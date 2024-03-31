3b8d95be82cec0f951a080f0f55d23423202c4a7
Ravimaya <03230239.gcbs@rub.edu.bt>
2024-03-31 20:29:22 +0600

    first

---
+++ stringsEx.py
@@ -0,0 +1,8 @@
+# get user input
+# convert it into ALL upper case
+# print the converted string
+
+
+userInput = input("Enter a string: ")
+userInputConverted = userInput.upper()
+print(userInputConverted)
